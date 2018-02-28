59eae085728552ebec9f5d90cfcaa2783aa7593b
Bob Habig <rfhab2@gmail.com>
2018-02-28 06:05:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function theBeatlesPlay(musicians, instruments){
   var array = [];
   for (let i = 0; i < musicians.length; i++) {
     
-    var string = `${musicians[i]} plays ${instruments[i]}`;
-    array.push(string);
+    
+    array.push(musicians[i] + ' plays ' + instruments[i]);
   }
   return array;
 }
